d603fa409048af4f815f475ec0014600f88c83fb
song-of-songs <zypsalms@sina.com>
2022-11-18 10:58:14 +0800

    更新

---
+++ src/components/FileUpload.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>文件上传
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FileUpload',
+  data () {
+    return {
+      msg: 'hello!'
+    }
+  }
+}
+</script>
+<style scoped>
+div{
+  color: red;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import FileUpload from '@/components/FileUpload'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'FileUpload',
+      component: FileUpload
     }
   ]
 })
